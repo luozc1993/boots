83e3e1e68dd328bd6c80cffb6dc5953ede0038b7
lzc <1079648153@qq.com>
2018-12-19 15:01:20 +0800

    boots

--- eureka_client/src/main/java/cn/luozc/EurekaClientApplication.java
+++ eureka_client/src/main/java/cn/luozc/EurekaClientApplication.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
-public class EurekaClientApplication {
-
+public class EurekaClientApplication extends SpringBootServletInitializer {
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(EurekaClientApplication.class);
+//    }
     public static void main(String[] args) {
         SpringApplication.run(EurekaClientApplication.class, args);
     }
--- eureka_server/src/main/java/cn/luozc/EurekaServerApplication.java
+++ eureka_server/src/main/java/cn/luozc/EurekaServerApplication.java
@@ -2,11 +2,17 @@ package cn.luozc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaServerApplication {
+public class EurekaServerApplication extends SpringBootServletInitializer {
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(EurekaServerApplication.class);
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
