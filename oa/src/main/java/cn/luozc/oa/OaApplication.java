c5e86db0b455fd5270feeff7ad3584db0ca74b43
lzc <1079648153@qq.com>
2018-12-21 13:15:15 +0800

    ss

---
+++ oa/src/main/java/cn/luozc/oa/OaApplication.java
@@ -0,0 +1,14 @@
+package cn.luozc.oa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OaApplication.class, args);
+    }
+
+}
+
